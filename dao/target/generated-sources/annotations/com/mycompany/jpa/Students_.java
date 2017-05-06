47cb0b1ffd5fcfb9d0847500c0af6b6c6969936b
perazzo <giovanniperazzo07@gmail.com>
2017-05-06 13:48:18 -0500

    Project Commit

---
+++ dao/target/generated-sources/annotations/com/mycompany/jpa/Students_.java
@@ -0,0 +1,16 @@
+package com.mycompany.jpa;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-05-05T18:37:18")
+@StaticMetamodel(Students.class)
+public class Students_ { 
+
+    public static volatile SingularAttribute<Students, String> firstname;
+    public static volatile SingularAttribute<Students, Integer> id;
+    public static volatile SingularAttribute<Students, Integer> age;
+    public static volatile SingularAttribute<Students, String> lastname;
+
+}
